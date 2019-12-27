2e2560f8815e167459f53c9de8d5c1eceaffc714
cschweda <cschweda@gmail.com>
2019-12-27 13:58:06 -0600

    fix: manually add redirects

--- generateRedirects.js
+++ generateRedirects.js
@@ -1,19 +1,19 @@
-const routes = require('./routes')
-const fs = require('fs')
+// const routes = require('./routes')
+// const fs = require('fs')
 
-//console.log(routes)
-let contents = '/\t/index.html\t200\n'
-routes.forEach(route => {
-  if (route !== '/') {
-    contents = contents + `${route}\t${route}/index.html\t200\n`
-  }
-})
+// //console.log(routes)
+// let contents = '/\t\t/index.html\t\t200\n'
+// routes.forEach(route => {
+//   if (route !== '/') {
+//     contents = contents + `${route}\t\t${route}/index.html\t\t200\n`
+//   }
+// })
 
-try {
-  fs.writeFileSync('./static/_redirects', contents)
-} catch (err) {
-  // An error occurred
-  console.error(err)
-}
+// try {
+//   fs.writeFileSync('./static/_redirects', contents)
+// } catch (err) {
+//   // An error occurred
+//   console.error(err)
+// }
 
-console.log('_redirects generated')
+// console.log('_redirects generated')
