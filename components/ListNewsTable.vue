5d1b9c00b839f18cdee4e2f4f25172f9722a4ad5
cschweda <cschweda@gmail.com>
2019-12-30 11:33:40 -0600

    fix: add simple table for temp  mobile display

--- components/ListNewsTable.vue
+++ components/ListNewsTable.vue
@@ -2,60 +2,98 @@
   <div>
     <client-only>
       <Loader slot="placeholder" />
-      <v-card color="white">
-        <v-card-title>
-          <v-spacer />
-          <v-text-field
-            v-model="search"
+      <div v-if="!showSimpleTable">
+        <v-card color="white">
+          <v-card-title>
+            <v-spacer />
+            <v-text-field
+              v-model="search"
           
-            label="Search"
-            single-line
-            hide-details
-            class="mb-10"
-          />
-        </v-card-title>
-        <v-data-table
-          :headers="headers"
-          :items="items"
-          :items-per-page="-1"
-          :search="search"
-          class="elevation-1 newsTable hover"
-          show-expand
-          item-key="slug"
-          :single-expand="singleExpand"
-          :expanded.sync="expanded"
-          @click:row="clicked"
-        >
-          <template
-            v-slot:item.createdAt="{ item }"
+              label="Search"
+              single-line
+              hide-details
+              class="mb-10"
+            />
+          </v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="items"
+            :items-per-page="-1"
+            :search="search"
+            class="elevation-1 newsTable hover"
+            show-expand
+            item-key="slug"
+            :single-expand="singleExpand"
+            :expanded.sync="expanded"
+            @click:row="clicked"
           >
-            <div style="cursor: pointer !important">
-              {{ item.posted | format }}
-            </div>
-          </template>
+            <template
+              v-slot:item.createdAt="{ item }"
+            >
+              <div style="cursor: pointer !important">
+                {{ item.posted | format }}
+              </div>
+            </template>
 
         
 
-          <template
-            v-slot:item.title="{ item }"
-          >
-            <b class="hover">{{ item.title }}</b>
-          </template>
+            <template
+              v-slot:item.title="{ item }"
+            >
+              <b class="hover">{{ item.title }}</b>
+            </template>
 
-          <template v-slot:expanded-item="{ headers, item }">
-            <td :colspan="headers.length + 1">
-              <div class="py-5">
-                <NewsCard
-                  :content="item"
+            <template v-slot:expanded-item="{ headers, item }">
+              <td :colspan="headers.length + 1">
+                <div class="py-5">
+                  <NewsCard
+                    :content="item"
                 
-                  :elevation="true"
-                  :display-news-link="true"
-                />
-              </div>
-            </td>
+                    :elevation="true"
+                    :display-news-link="true"
+                  />
+                </div>
+              </td>
+            </template>
+          </v-data-table>
+        </v-card>
+      </div>
+      <div v-else>
+        <v-simple-table fixed-header>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">
+                  Posted
+                </th>
+                <th class="text-left">
+                  Title
+                </th>
+                <th class="text-left">
+                  Link
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="item in items"
+                :key="item.title"
+                @click.stop="$router.push(`/news/${item.slug}`)"
+              >
+                <td>{{ item.posted | format }}</td>
+                <td>{{ item.title }}</td>
+                <td>
+                  <v-btn>
+                    <v-icon @click.stop="$router.push(`/news/${item.slug}`)">
+                      link
+                    </v-icon>
+                  </v-btn>
+                </td>
+              </tr>
+            </tbody>
           </template>
-        </v-data-table>
-      </v-card>
+        </v-simple-table>
+      </div>
     </client-only>
   </div>
 </template>
@@ -94,6 +132,15 @@ export default {
         },
         { text: 'Title', value: 'title' }
       ]
+    }
+  },
+  computed: {
+    showSimpleTable() {
+      if (this.$vuetify.breakpoint.sm || this.$vuetify.breakpoint.xs) {
+        return true
+      } else {
+        return false
+      }
     }
   },
   mounted() {
